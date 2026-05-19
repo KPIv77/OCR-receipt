b310a2c618df103edc84b339220a15bc442e71fe
KPIv77 <kitisakpholor@gmail.com>
2026-05-20 06:24:17 +0700

    Move all congig and path to main.py

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import torch
+from classify_receipt import ReceiptClassifier
+from receipt_read import ReceiptRead
+
+# Config
+IMG_SIZE = 224
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+CLASS_NAMES = ["Kbank", "NEXT", "SCB"]
+
+# Path model and image
+model_receipt = r"/home/kph/project/OCR-receipt/model/receipt_model.pth"
+image_path = r"/home/kph/project/OCR-receipt/receipt/218324.jpg"
+
+if __name__ == "__main__":
+    
+    # Config file classify_receipt.py 
+    classifier = ReceiptClassifier(
+        model_path=model_receipt,
+        class_names=CLASS_NAMES,
+        img_size=IMG_SIZE,
+        device=DEVICE,
+    )
+
+    result = classifier.predict(image_path)
+
+    # Print check
+    print(result)
+    print(result["bank"])
+    print(result["confidence"])
+
+    receipt_read = ReceiptRead()
+    print(receipt_read.bank(result))
