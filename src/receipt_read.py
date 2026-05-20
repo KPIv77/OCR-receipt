f1aed122e5234166d4da670b880525b555694291
KPIv77 <kitisakpholor@gmail.com>
2026-05-20 18:22:58 +0700

    demo check bank

--- src/receipt_read.py
+++ src/receipt_read.py
@@ -1,8 +1,30 @@
+import easyocr
+
 class ReceiptRead:
 
-    def bank(self, result: dict) -> str:
+    # Read receipt
+    def __init__(self, lang: list[str] = ["th", "en"], model_dir: str = None, download_enabled: bool = True):
+        self.reader = easyocr.Reader(
+            lang, 
+            model_storage_directory=model_dir,
+            download_enabled=download_enabled)
+
+    def bank_Kb(self, img_path: str) -> str:
+        results = self.reader.readtext(img_path)
+        text = "\n".join([item[1] for item in results])
+        return text
+    
+    def bank_Sc():
+        pass
+    
+    def Nx():
+        pass
+    
+    # Check bank
+    def bank(self, result: dict, img_path: str) -> str:
+        
         if result["bank"] == "Kbank":
-            return "Kbank"
+            return self.bank_Kb(img_path=img_path)
         
         elif result["bank"] == "NEXT":
             return "NEXT"
